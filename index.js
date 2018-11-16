7e206f5a0c2bf38f2196b7ecd4464714f43bfd87
zhangbin <zhangbin05@qiyi.com>
2018-11-16 19:55:14 +0800

    feat: basic structure.

---
+++ index.js
@@ -0,0 +1,51 @@
+const parse = require('csv-parse/lib/sync')
+
+const fs = require('fs')
+
+const input = fs.readFileSync('./history.csv')
+
+const records = parse(input, {
+    columns: true,
+    skip_empty_lines: true
+})
+console.log(records);
+
+let history = []
+
+history[0] = {
+    "pid": 1,
+    "tid": 1,
+    "ts": 1,
+    "ph": "X"
+}
+const tidMap = {
+    'war': 1,
+    'society': 2
+}
+records.forEach(record => {
+    const start = record.start.trim(), end = record.end.trim(), tid = tidMap[record.type.trim()]
+    if (end === '-') {
+        history.push({
+            "pid": 1,
+            "tid": tid,
+            "ts": ymd2t(start),
+            "ph": "I",
+            "name": record.name
+        })
+    } else {
+        history.push({
+            "pid": 1,
+            "tid": tid,
+            "ts": ymd2t(start),
+            "name": record.name,
+            "dur": ymd2t(end) - ymd2t(start)
+        })
+    }
+})
+
+function ymd2t(ymd) {
+    const year = +ymd.substr(0, 4), month = +ymd.substr(4, 2), day = +ymd.substr(6, 2)
+    return +Number((year + month / 12 + day / 30 / 12) * 1000).toFixed(0)
+}
+console.log(history);
+fs.writeFileSync('./history.json', JSON.stringify(history))
