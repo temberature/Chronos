b747f88c9d372033f475444de7a0fc412e2d724f
TongZhang <temberature@gmail.com>
2018-11-18 11:13:27 +0800

    feat: 支持多维度。

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const parse = require('csv-parse/lib/sync')
 
 const fs = require('fs')
 
-const input = fs.readFileSync('./music_art_history.CSV')
+const input = fs.readFileSync('./history.csv')
 
 const records = parse(input, {
     columns: true,
@@ -20,7 +20,12 @@ history[0] = {
 }
 const tidMap = {
     'war': 1,
-    'society': 2
+    'political': 2,
+    'statesman': 3,
+    'technology': 4,
+    'economic': 5,
+    'religion': 6,
+    'society': 7,
 }
 records.forEach(record => {
     const start = record.start.trim(), end = record.end.trim(), tid = tidMap[record.type.trim()]
@@ -30,7 +35,7 @@ records.forEach(record => {
             "tid": tid,
             "ts": ymd2t(start),
             "ph": "I",
-            "name": record.Name
+            "name": record.name
         })
     } else {
         const dur = ymd2t(end) - ymd2t(start);
@@ -46,7 +51,8 @@ records.forEach(record => {
 
 function ymd2t(ymd) {
     const year = +ymd.substr(0, 4), month = +ymd.substr(4, 2), day = +ymd.substr(6, 2)
-    return +Number((year + month / 12 + day / 30 / 12) * 1000).toFixed(0)
+    let finalYear = month === 0 ? (year + 0.5) : year + month / 12 + day / 30 / 12
+    return +Number(finalYear * 1000).toFixed(0)
 }
 console.log(history);
 fs.writeFileSync('./history.json', JSON.stringify(history))
